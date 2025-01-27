089b4979b17aaaf9536416f8c7f83005a0cdee00
JinsolSong <jeanies@naver.com>
2025-01-27 14:35:11 +0900

    fix : 과제9 번       Spring security 수정
          주석처리 잘못한 부분 해제

--- src/main/java/org/example/expert/config/JwtFilter.java
+++ src/main/java/org/example/expert/config/JwtFilter.java
@@ -1,6 +1,9 @@
 package org.example.expert.config;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -49,7 +52,7 @@ public class JwtFilter implements Filter {
 
         String jwt = jwtUtil.substringToken(bearerJwt);
 
-//        try {
+        try {
             // JWT 유효성 검사와 claims 추출
             Claims claims = jwtUtil.extractClaims(jwt);
             if (claims == null) {
@@ -92,19 +95,19 @@ public class JwtFilter implements Filter {
 //            }
 
             chain.doFilter(request, response);
-//        } catch (SecurityException | MalformedJwtException e) {
-//            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.", e);
-//            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
-//        } catch (ExpiredJwtException e) {
-//            log.error("Expired JWT token, 만료된 JWT token 입니다.", e);
-//            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 JWT 토큰입니다.");
-//        } catch (UnsupportedJwtException e) {
-//            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.", e);
-//            httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "지원되지 않는 JWT 토큰입니다.");
-//        } catch (Exception e) {
-//            log.error("Internal server error", e);
-//            httpResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//        }
+        } catch (SecurityException | MalformedJwtException e) {
+            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.", e);
+            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.error("Expired JWT token, 만료된 JWT token 입니다.", e);
+            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.", e);
+            httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "지원되지 않는 JWT 토큰입니다.");
+        } catch (Exception e) {
+            log.error("Internal server error", e);
+            httpResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
